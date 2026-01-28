0dac16e7f12e2b6d4ebc01b8ae9d680f528d607b
ArmitaFarhadi <mohanna9069@gmail.com>
2026-01-28 17:28:16 +1100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,10 +2,12 @@ window._config = {
     cognito: {
         userPoolId: 'ap-southeast-2_m40WWbVSe', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6iuk38vscm5pde21bsgej56je7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-southeast-2' // e.g. us-east-2
+        region: 'ap-southeast-2', // e.g. us-east-2
+        userPoolClientSecret:'7652vqn9o8mglt1d9g3d0j9hd5tu9dmjv56vrhacrukcedafaf2'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 

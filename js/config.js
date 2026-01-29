5f763d914d351b45e045a98bc090e6edf32ed6f1
ArmitaFarhadi <mohanna9069@gmail.com>
2026-01-29 14:29:40 +1100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-southeast-2_m40WWbVSe', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6iuk38vscm5pde21bsgej56je7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3jk282cla2s9i4ec07dmo7jg0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
